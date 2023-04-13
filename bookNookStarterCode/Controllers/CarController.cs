9b835640225025af98e5891bc6fc40945e2641a9
Daniel Tulpa <dtulpa16@yahoo.com>
2023-04-13 15:20:32 -0600

    Added Notes In CarController

--- bookNookStarterCode/Controllers/CarController.cs
+++ bookNookStarterCode/Controllers/CarController.cs
@@ -30,11 +30,15 @@ namespace bookNookStarterCode.Controllers
         {
             try
             {
+                // Retrieve all cars from the database, including the owner object
                 var cars = _context.Cars.Include(u => u.Owner);
+
+                // Return the list of cars as a 200 OK response
                 return StatusCode(200, cars);
             }
             catch (Exception ex)
             {
+                // If an error occurs, return a 500 internal server error with the error message
                 return StatusCode(500, ex.Message);
             }
         }
@@ -43,12 +47,18 @@ namespace bookNookStarterCode.Controllers
         {
             try
             {
+                // Retrieve the authenticated user's ID from the JWT token
                 string userId = User.FindFirstValue("id");
+
+                // Retrieve all cars that belong to the authenticated user, including the owner object
                 var cars = _context.Cars.Include(u => u.Owner).Where(c => c.OwnerId.Equals(userId));
+
+                // Return the list of cars as a 200 OK response
                 return StatusCode(200, cars);
             }
             catch (Exception ex)
             {
+                // If an error occurs, return a 500 internal server error with the error message
                 return StatusCode(500, ex.Message);
             }
         }
@@ -58,15 +68,21 @@ namespace bookNookStarterCode.Controllers
         {
             try
             {
+                // Retrieve the car with the specified ID, including the owner object
                 var car = _context.Cars.Include(c => c.Owner).FirstOrDefault(c => c.Id == id);
+
+                // If the car does not exist, return a 404 not found response
                 if (car == null)
                 {
                     return NotFound();
                 }
+
+                // Return the car as a 200 OK response
                 return StatusCode(200, car);
             }
             catch (Exception ex)
             {
+                // If an error occurs, return a 500 internal server error with the error message
                 return StatusCode(500, ex.Message);
             }
         }
@@ -77,24 +93,34 @@ namespace bookNookStarterCode.Controllers
         {
             try
             {
+                // Retrieve the authenticated user's ID from the JWT token
                 string userId = User.FindFirstValue("id");
+
+                // If the user ID is null or empty, the user is not authenticated, so return a 401 unauthorized response
                 if (string.IsNullOrEmpty(userId))
                 {
                     return Unauthorized();
                 }
 
+                // Set the car's owner ID and owner object to the authenticated user
                 data.OwnerId = userId;
                 data.Owner = _context.Users.Find(userId);
+
+                // Add the car to the database and save changes
                 _context.Cars.Add(data);
                 if (!ModelState.IsValid)
                 {
+                    // If the car model state is invalid, return a 400 bad request response with the model state errors
                     return BadRequest(ModelState);
                 }
                 _context.SaveChanges();
+
+                // Return the newly created car as a 201 created response
                 return StatusCode(201, data);
             }
             catch (Exception ex)
             {
+                // If an error occurs, return a 500 internal server error with the error message
                 return StatusCode(500, ex.Message);
             }
         }
@@ -105,10 +131,12 @@ namespace bookNookStarterCode.Controllers
         {
             try
             {
+                // Find the car to be updated
                 Car car = _context.Cars.Include(c => c.Owner).FirstOrDefault(c => c.Id == id);
 
                 if (car == null)
                 {
+                    // Return a 404 Not Found error if the car with the specified ID does not exist
                     return NotFound();
                 }
 
@@ -116,6 +144,7 @@ namespace bookNookStarterCode.Controllers
                 var userId = User.FindFirstValue("id");
                 if (string.IsNullOrEmpty(userId) || car.OwnerId != userId)
                 {
+                    // Return a 401 Unauthorized error if the authenticated user is not the owner of the car
                     return Unauthorized();
                 }
 
@@ -127,14 +156,17 @@ namespace bookNookStarterCode.Controllers
                 car.Year = data.Year;
                 if (!ModelState.IsValid)
                 {
+                    // Return a 400 Bad Request error if the request data is invalid
                     return BadRequest(ModelState);
                 }
                 _context.SaveChanges();
 
+                // Return a 201 Created status code and the updated car object
                 return StatusCode(201, car);
             }
             catch (Exception ex)
             {
+                // Return a 500 Internal Server Error with the error message if an exception occurs
                 return StatusCode(500, ex.Message);
             }
         }
@@ -145,31 +177,32 @@ namespace bookNookStarterCode.Controllers
         {
             try
             {
+                // Find the car to be deleted
                 Car car = _context.Cars.FirstOrDefault(c => c.Id == id);
                 if (car == null)
                 {
+                    // Return a 404 Not Found error if the car with the specified ID does not exist
                     return NotFound();
                 }
+
                 // Check if the authenticated user is the owner of the car
                 var userId = User.FindFirstValue("id");
                 if (string.IsNullOrEmpty(userId) || car.OwnerId != userId)
                 {
+                    // Return a 401 Unauthorized error if the authenticated user is not the owner of the car
                     return Unauthorized();
                 }
 
-                if (car == null)
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    _context.Cars.Remove(car);
-                    _context.SaveChanges();
-                    return StatusCode(204);
-                }
+                // Remove the car from the database
+                _context.Cars.Remove(car);
+                _context.SaveChanges();
+
+                // Return a 204 No Content status code
+                return StatusCode(204);
             }
             catch (Exception ex)
             {
+                // Return a 500 Internal Server Error with the error message if an exception occurs
                 return StatusCode(500, ex.Message);
             }
         }
